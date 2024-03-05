067971b4f36c4aa6f3d2557ef9edd088023371e1
ajhalim <thenubbum@gmail.com>
2024-03-04 22:25:57 -0800

    abeSketech

---
+++ pages/pageThree/sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+    createCanvas(550, 550);
+    strokeCap(SQUARE);
+  }
+  
+  function draw() {
+    background(255);
+    translate(width / 2, height / 2);
+  
+    var circleResolution = int(map(mouseY, 0, height, 2, 80));
+    var radius = mouseX - width / 2;
+    var angle = TAU / circleResolution;
+  
+    fill(mouseX, mouseY, mouseX/2);
+    var thing = noise(mouseY)
+    strokeWeight(thing / 200);
+  
+    for (var i = 0; i <= circleResolution; i++) {
+      var x = cos(angle * i) * radius;
+      var y = sin(angle * i) * radius;
+      rect(0, 0, x, y);
+    }
+  }
+  
